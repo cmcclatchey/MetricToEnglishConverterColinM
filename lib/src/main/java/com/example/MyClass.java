0da83c88a1325f9ceef5bc46f96f7a152e0b1979
cmcclatchey <cmcclatchey@e104-m-16.mchenry.edu>
2015-09-15 13:06:40 -0500

    Initial commit

---
+++ lib/src/main/java/com/example/MyClass.java
@@ -0,0 +1,35 @@
+package com.example;
+
+public class MyClass
+{
+    public static void main(String[] args)
+    {
+        double metersToFeet;
+        double kilometersToFeet;
+        double centimetersToInches;
+        double kphToFps;
+        double litersToGallons;
+        double kilogramsToPounds;
+
+
+        metersToFeet = 28 * 3.281;
+        kilometersToFeet = 168 * 3280.84;
+        centimetersToInches = 47 * .394;
+        kphToFps = 27 * .911;
+        litersToGallons = 92 * .264;
+        kilogramsToPounds = 200 * 2.205;
+
+
+        System.out.println("28 meters is " + metersToFeet + " feet.");
+        System.out.println("168 kilometers is " + kilometersToFeet + " feet.");
+        System.out.println("47 centimeters is " + centimetersToInches + " inches.");
+        System.out.println("27 kilometers per hour is " + kphToFps + " feet per second.");
+        System.out.println("92 liters is " + litersToGallons + " gallons.");
+        System.out.println("200 kilograms is " + kilogramsToPounds + " pounds.");
+
+
+
+
+
+    }
+}
